39b1b317392e6c00f92a55f93ed45e099182b9c8
VladimirStozhkov <vstozhkov@ufl.edu>
2014-09-21 18:12:39 -0400

    I have inserted my code for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,48 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Vladimir Stozhkov's comments: 
+## I prefer to use a capital 'X' instead of 'x' because
+## capital letters are associated with matrices whereas lowercase letters correspond to vectors.
+## invM is a cached inverse matrix.
+## Function 'set' copies an input matrix into 'X'.
+## Function 'get' outputs a stored matrix from cache.
+## Function 'setInvMatrix' computes an inverse matrix of a stored 'X'.
+## Function 'getInvMatrix' shows an inverse matrix stored in cache.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(X = matrix()) 
+{
+    invM <- NULL
+    set <- function(Y) 
+    {
+        X <<- Y
+        invM <<- NULL
+    }
+    get <- function() X
+    setInvMatrix <- function(invMatrix) invM <<- invMatrix
+    getInvMatrix <- function() invM
+    list(set = set, get = get,
+         setInvMatrix = setInvMatrix,
+         getInvMatrix = getInvMatrix) 
 }
 
 
 ## Write a short comment describing this function
+## Vladimir Stozhkov's comments:
+## Function 'cacheSolve' first checks if there is an inverse matrix of the object 'X' stored in cache.
+## If there is not the function calls a standard function 'solve' to calculate it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(X, ...) 
+{
+    ## Return a matrix that is the inverse of 'X'
+    invM <- X$getInvMatrix()
+    if(!is.null(invM))
+    {
+        message("getting a cached inverse matrix")
+        return(invM)
+    }
+    data <- X$get()
+    invM <- solve(data, ...)
+    X$setInvMatrix(invM)
+    invM
 }
